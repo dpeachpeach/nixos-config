12ff8cec1f8f76a7a972ed96c8c4612486577c83
David Petre <davidpetre@uchicago.edu>
2024-06-01 18:44:13 -0500

    added signing key

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -3,6 +3,12 @@
 		enable = true;
 		userName = "David Petre";
 		userEmail = "davidpetre@uchicago.edu";
+		extraConfig = {
+			user.signingKey =  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEf9vNJ+4BAtlKhRPZL7myDhiew3NydQn4jkLXNTQpYV david@david-nuc";
+			gpg.format = "ssh";
+			commit.gpgsign = true;
+			tag.gpgsign = true;
+		};
 
 	};
 }
